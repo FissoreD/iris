6f40f593c8b75a9bf2f236b9d59ea23541f27886
Ralf Jung <jung@mpi-sws.org>
2023-05-25 05:50:38 +0000

    This seems to work, thanks!

--- iris/bi/plainly.v
+++ iris/bi/plainly.v
@@ -674,8 +674,7 @@ Section plainly_derived.
   Qed.
 
 Lemma fixpoint_plain_absorbing {A} (F : (A -d> bi_ofeO PROP) -> A -d> bi_ofeO PROP) `{Contractive F}:
-    (∀ Φ, (∀ x, Plain (Φ x)) → (∀ x, Absorbing (Φ x)) → (∀ x, Plain (F Φ x))) →
-    (∀ Φ, (∀ x, Plain (Φ x)) → (∀ x, Absorbing (Φ x)) → (∀ x, Absorbing (F Φ x))) →
+    (∀ Φ, (∀ x, Plain (Φ x)) → (∀ x, Absorbing (Φ x)) → (∀ x, Plain (F Φ x) ∧ Absorbing (F Φ x))) →
     ∀ x, Plain (fixpoint F x) ∧ Absorbing (fixpoint F x).
 Proof.
   intros ??.
