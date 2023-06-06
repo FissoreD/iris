3d264acf920580c052fb8845a41a5b802b36a849
Ike Mulder <ikemulder@hotmail.com>
2023-06-06 11:19:57 +0200

    Remove reservation_map stuff

--- iris/base_logic/lib/gen_heap.v
+++ iris/base_logic/lib/gen_heap.v
@@ -214,7 +214,7 @@ Section gen_heap.
     rewrite meta_token_unseal /meta_token_def.
     iIntros "(%γm1 & #Hγm1 & Hm1) (%γm2 & #Hγm2 & Hm2)".
     iCombine "Hγm1 Hγm2" gives %[_ ->].
-    iCombine "Hm1 Hm2" gives %?.
+    iCombine "Hm1 Hm2" gives %?%reservation_map_token_valid_op.
     iExists γm2. iFrame "Hγm2". rewrite reservation_map_token_union //. by iSplitL "Hm1".
   Qed.
   Lemma meta_token_union l E1 E2 :
@@ -237,7 +237,9 @@ Section gen_heap.
     rewrite meta_unseal /meta_def.
     iIntros "(%γm1 & Hγm1 & Hm1) (%γm2 & Hγm2 & Hm2)".
     iCombine "Hγm1 Hγm2" gives %[_ ->].
-    iCombine "Hm1 Hm2" gives %Hγ. naive_solver.
+    iCombine "Hm1 Hm2" gives %Hγ. iPureIntro.
+    move: Hγ. rewrite -reservation_map_data_op reservation_map_data_valid.
+    move=> /to_agree_op_inv_L. naive_solver.
   Qed.
   Lemma meta_set `{Countable A} E l (x : A) N :
     ↑ N ⊆ E → meta_token l E ==∗ meta l N x.
