8178efc8aee627e9331385fc8ab23070f376be29
Jan-Oliver Kaiser <janno@bedrocksystems.com>
2024-03-21 22:30:15 +0100

    iFrame: Do not call `cbv` on user terms in contexts

--- iris/proofmode/ltac_tactics.v
+++ iris/proofmode/ltac_tactics.v
@@ -410,7 +410,7 @@ Ltac iFrameAnyIntuitionistic :=
     match Hs with [] => idtac | ?H :: ?Hs => repeat iFrameHyp H; go Hs end in
   match goal with
   | |- envs_entails ?Δ _ =>
-     let Hs := eval cbv in (env_dom (env_intuitionistic Δ)) in go Hs
+     let Hs := eval lazy in (env_dom (env_intuitionistic Δ)) in go Hs
   end.
 
 Ltac iFrameAnySpatial :=
@@ -419,7 +419,7 @@ Ltac iFrameAnySpatial :=
     match Hs with [] => idtac | ?H :: ?Hs => try iFrameHyp H; go Hs end in
   match goal with
   | |- envs_entails ?Δ _ =>
-     let Hs := eval cbv in (env_dom (env_spatial Δ)) in go Hs
+     let Hs := eval lazy in (env_dom (env_spatial Δ)) in go Hs
   end.
 
 Local Ltac _iFrame_go Hs :=
